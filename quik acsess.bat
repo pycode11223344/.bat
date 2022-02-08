5712a9f2ab7b779ef4b16a917b2f0d31d82d8151
pycode11223344 <95154432+pycode11223344@users.noreply.github.com>
2022-02-08 16:15:34 +1100

    Add files via upload

---
+++ quik acsess.bat
@@ -0,0 +1,41 @@
+echo off
+title quiz
+color 0a
+cls
+pause
+:MENU
+ECHO.
+ECHO ...............................................
+ECHO PRESS 1, 2 OR 3 to select your task, or 4 to EXIT.
+ECHO ...............................................
+ECHO.
+ECHO 1 - Open Notepad
+ECHO 2 - Open Calculator
+ECHO 3 - Open Notepad AND Calculator
+ECHO 4 - EXIT
+ECHO.
+
+
+SET /P M=Type 1, 2, 3, or 4 then press ENTER:
+IF %M%==1 GOTO NOTE
+IF %M%==2 GOTO CALC
+IF %M%==3 GOTO BOTH
+IF %M%==4 GOTO EOF
+
+:NOTE
+cd %windir%\system32\notepad.exe
+start notepad.exe
+cls
+GOTO MENU
+:CALC
+cd %windir%\system32\calc.exe
+start calc.exe
+cls
+GOTO MENU
+:BOTH
+cd %windir%\system32\notepad.exe
+start notepad.exe
+cd %windir%\system32\calc.exe
+start calc.exe
+cls
+GOTO MENU
